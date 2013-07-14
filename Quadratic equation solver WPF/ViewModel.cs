27b7b26f5f528e86ced5a417dc2c07bb87c4d549
JohnnyPP <johnnnyppp@gmail.com>
2013-07-14 03:07:25 +0200

    Menu->Exit with Application.Current.Shutdown();

--- Quadratic equation solver WPF/ViewModel.cs
+++ Quadratic equation solver WPF/ViewModel.cs
@@ -5,116 +5,132 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 using System.Windows.Threading;
 
 namespace Quadratic_equation_solver_WPF
 {
-    class ViewModel : INotifyPropertyChanged
-    {
-        
-        Solver solve = new Solver();
-        private DateTime _now;
+	class ViewModel : INotifyPropertyChanged
+	{
+		
+		Solver solve = new Solver();
 
-        public ViewModel()
-	    {
-            DispatcherTimer dispatcherTimer = new DispatcherTimer();
-            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
-            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
-            dispatcherTimer.Start();
-	    }
+		public ViewModel()
+		{
+			DispatcherTimer dispatcherTimer = new DispatcherTimer();
+			dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+			dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+			dispatcherTimer.Start();
+		}
 
-        private void dispatcherTimer_Tick(object sender, EventArgs e)
-        {
-            
-                TimeCommand = DateTime.Now;
-              
-        }
+		private void dispatcherTimer_Tick(object sender, EventArgs e)
+		{
+			
+			TimeCommand = DateTime.Now;
+			  
+		}
 
-        private string updateTextBox;
+		private string updateTextBox;
 
-        public string UpdateTextBox
-        {
-            get { return updateTextBox; }
-            set
-            {
-                if (updateTextBox != value)
-                {
-                    updateTextBox = value;
-                    RaisePropertyChanged("UpdateTextBox");      
-                }                                              
-                updateTextBox = value;                          
-            }                                                    
-        }
+		public string UpdateTextBox
+		{
+			get { return updateTextBox; }
+			set
+			{
+				if (updateTextBox != value)
+				{
+					updateTextBox = value;
+					RaisePropertyChanged("UpdateTextBox");      
+				}                                              
+				updateTextBox = value;                          
+			}                                                    
+		}
 
-        private string updateLabel;
+		private string updateLabel;
 
-        public string UpdateLabel
-        {
-            get { return updateLabel; }
-            set
-            {
-                if (updateLabel != value)
-                {
-                    updateLabel = value;
-                    RaisePropertyChanged("UpdateLabel");
-                }
-                updateLabel = value;
-            }
-        }
+		public string UpdateLabel
+		{
+			get { return updateLabel; }
+			set
+			{
+				if (updateLabel != value)
+				{
+					updateLabel = value;
+					RaisePropertyChanged("UpdateLabel");
+				}
+				updateLabel = value;
+			}
+		}
 
-        public event PropertyChangedEventHandler PropertyChanged;
+		public event PropertyChangedEventHandler PropertyChanged;
 
-        private void RaisePropertyChanged(string propertyName)
-        {
-            // take a copy to prevent thread issues
-            PropertyChangedEventHandler handler = PropertyChanged;
-            if (handler != null)
-            {
-                handler(this, new PropertyChangedEventArgs(propertyName));
-            }
-        }
+		private void RaisePropertyChanged(string propertyName)
+		{
+			// take a copy to prevent thread issues
+			PropertyChangedEventHandler handler = PropertyChanged;
+			if (handler != null)
+			{
+				handler(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
 
-        #region Commands
-        void UpdateLabelExecute()
-        {
+		#region Commands
+		void UpdateLabelExecute()
+		{
 
-            UpdateLabel = solve.Results(UpdateTextBox);
-        }
+			UpdateLabel = solve.Results(UpdateTextBox);
+		}
 
-        bool CanUpdateLabelExecute()
-        {
-            return true;
-        }
+		bool CanUpdateLabelExecute()
+		{
+			return true;
+		}
 
-        public ICommand UpdateLabelButtonClick
-        {
-            get { return new RelayCommand(UpdateLabelExecute, CanUpdateLabelExecute); }
-        }
-        #endregion
+		public ICommand UpdateLabelButtonClick
+		{
+			get { return new RelayCommand(UpdateLabelExecute, CanUpdateLabelExecute); }
+		}
+		
+
+		void ExitApplicationCommand()
+		{
+			Application.Current.Shutdown();
+		}
+
+		bool CanExitApplicationExecute()
+		{
+			return true;
+		}
+
+		public ICommand ExitApplicationClick  //bind to "ExitApplicationClick" name in XAML
+		{
+			get { return new RelayCommand(ExitApplicationCommand, CanExitApplicationExecute); }
+		}
+
+		#endregion
+
+		///////////////////////////////////////DateTime
+		#region DateTime
 
 
-        ///////////////////////////////////////DateTime
-        #region DateTime
 
+		private DateTime timeCommand;
 
+		public DateTime TimeCommand
+		{
+			get { return timeCommand; }
+			set
+			{
+				if (timeCommand != value)
+				{
+					timeCommand = value;
+					RaisePropertyChanged("TimeCommand");
+				}
+				timeCommand = value;
+			}
+		}
+		#endregion
 
-        private DateTime timeCommand;
-
-        public DateTime TimeCommand
-        {
-            get { return timeCommand; }
-            set
-            {
-                if (timeCommand != value)
-                {
-                    timeCommand = value;
-                    RaisePropertyChanged("TimeCommand");
-                }
-                timeCommand = value;
-            }
-        }
-        #endregion
-
-    }
+	}
 }
