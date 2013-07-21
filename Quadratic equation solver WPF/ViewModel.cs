1140d4ef582df8ba5b076bc057711c0ecdc8cda9
JohnnyPP <johnnnyppp@gmail.com>
2013-07-21 16:54:29 +0200

    Entity framework now is used for  writing history to to database

--- Quadratic equation solver WPF/ViewModel.cs
+++ Quadratic equation solver WPF/ViewModel.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Data.Entity;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -17,36 +18,36 @@ namespace Quadratic_equation_solver_WPF
 	class ViewModel : INotifyPropertyChanged
 	{
 
-        #region Fields
-        /// <summary>
-        /// Creates instance of the EquationSolver
-        /// </summary>
-        Solver solve = new Solver();
-        SolverTuple solveTuple = new SolverTuple();
+		#region Fields
+		/// <summary>
+		/// Creates instance of the EquationSolver
+		/// </summary>
+		Solver solve = new Solver();
+		SolverTuple solveTuple = new SolverTuple();
 
-        /// <summary>
+		/// <summary>
 		/// The time command
 		/// </summary>
 		private DateTime timeCommand;
 
-        /// <summary>
+		/// <summary>
 		/// The update text box
 		/// </summary>
 		private string updateTextBox;
 
-        /// <summary>
+		/// <summary>
 		/// The update label
 		/// </summary>
 		private string updateLabel;
 
-        /// <summary>
-        /// The index History
-        /// </summary>
-        int indexHistory = 1;
+		/// <summary>
+		/// The index History
+		/// </summary>
+		int indexHistory = 1;
 
-        #endregion
+		#endregion
 
-        #region Properties
+		#region Properties
 
 		/// <summary>
 		/// Gets or sets the time command.
@@ -110,87 +111,88 @@ namespace Quadratic_equation_solver_WPF
 
 		#endregion
 
-        #region Methods
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ViewModel"/> class.
-        /// </summary>
-        public ViewModel()
-        {
-            DispatcherTimer dispatcherTimer = new DispatcherTimer();
-            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
-            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
-            dispatcherTimer.Start();
-        }
+		#region Methods
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ViewModel"/> class.
+		/// </summary>
+		public ViewModel()
+		{
+			DispatcherTimer dispatcherTimer = new DispatcherTimer();
+			dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+			dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+			dispatcherTimer.Start();
+		}
 
-        /// <summary>
-        /// Handles the Tick event of the dispatcherTimer control.
-        /// </summary>
-        /// <param name="sender">The source of the event.</param>
-        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
-        private void dispatcherTimer_Tick(object sender, EventArgs e)
-        {
+		/// <summary>
+		/// Handles the Tick event of the dispatcherTimer control.
+		/// </summary>
+		/// <param name="sender">The source of the event.</param>
+		/// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+		private void dispatcherTimer_Tick(object sender, EventArgs e)
+		{
 
-            TimeCommand = DateTime.Now;
+			TimeCommand = DateTime.Now;
 
-        } 
-        #endregion
+		} 
+		#endregion
 
-        #region PropertyChanged
-        /// <summary>
-        /// Occurs when a property value changes.
-        /// </summary>
-        public event PropertyChangedEventHandler PropertyChanged;
+		#region PropertyChanged
+		/// <summary>
+		/// Occurs when a property value changes.
+		/// </summary>
+		public event PropertyChangedEventHandler PropertyChanged;
 
-        /// <summary>
-        /// Raises the property changed.
-        /// </summary>
-        /// <param name="propertyName">Name of the property.</param>
-        private void RaisePropertyChanged(string propertyName)
-        {
-            // take a copy to prevent thread issues
-            PropertyChangedEventHandler handler = PropertyChanged;
-            if (handler != null)
-            {
-                handler(this, new PropertyChangedEventArgs(propertyName));
-            }
-        } 
-        #endregion
+		/// <summary>
+		/// Raises the property changed.
+		/// </summary>
+		/// <param name="propertyName">Name of the property.</param>
+		private void RaisePropertyChanged(string propertyName)
+		{
+			// take a copy to prevent thread issues
+			PropertyChangedEventHandler handler = PropertyChanged;
+			if (handler != null)
+			{
+				handler(this, new PropertyChangedEventArgs(propertyName));
+			}
+		} 
+		#endregion
 
 		#region Commands
 
-        /// <summary>
-        /// Updates the label execute.
-        /// </summary>
+		/// <summary>
+		/// Updates the label execute.
+		/// </summary>
 		void UpdateLabelExecute()
 		{
 
 			//UpdateLabel = solve.Results(UpdateTextBox);
 
-            var tupleResults = solveTuple.ResultsTuple(UpdateTextBox);
-            UpdateLabel = tupleResults.Item1;
-            
-            listViewCollection.Add(new ListViewData { ListView_No = indexHistory.ToString(),
-                                                      ListView_a = solveTuple.a.ToString(),
-                                                      ListView_b = solveTuple.b.ToString(),
-                                                      ListView_c = solveTuple.c.ToString(),
-                                                      ListView_Discriminant = solveTuple.Discriminant.ToString(),
-                                                      ListView_x1 = tupleResults.Item2,
-                                                      ListView_x2 = tupleResults.Item3
-                                                    });
+			var tupleResults = solveTuple.ResultsTuple(UpdateTextBox);
+			UpdateLabel = tupleResults.Item1;
+			
+			listViewCollection.Add(new ListViewData { ListView_No = indexHistory.ToString(),
+													  ListView_a = solveTuple.a.ToString(),
+													  ListView_b = solveTuple.b.ToString(),
+													  ListView_c = solveTuple.c.ToString(),
+													  ListView_Discriminant = solveTuple.Discriminant.ToString(),
+													  ListView_x1 = tupleResults.Item2,
+													  ListView_x2 = tupleResults.Item3,
+                                                      ListView_Datetime = DateTime.Now
+													});
 
-            indexHistory++;
+			indexHistory++;
 
 
 		}
 
 
-        #region ExitApplicationClick
-        /// <summary>
-        /// Determines whether this instance [can update label execute].
-        /// </summary>
-        /// <returns>
-        ///   <c>true</c> if this instance [can update label execute]; otherwise, <c>false</c>.
-        /// </returns>
+		#region ExitApplicationClick
+		/// <summary>
+		/// Determines whether this instance [can update label execute].
+		/// </summary>
+		/// <returns>
+		///   <c>true</c> if this instance [can update label execute]; otherwise, <c>false</c>.
+		/// </returns>
 		bool CanUpdateLabelExecute()
 		{
 			if (!string.IsNullOrWhiteSpace(UpdateTextBox))  //disables the Calculate button or Solve menu item when UpdateTextBox is empty
@@ -204,103 +206,155 @@ namespace Quadratic_equation_solver_WPF
 			
 		}
 
-        /// <summary>
-        /// Gets the update label button click.
-        /// </summary>
-        /// <value>
-        /// The update label button click.
-        /// </value>
+		/// <summary>
+		/// Gets the update label button click.
+		/// </summary>
+		/// <value>
+		/// The update label button click.
+		/// </value>
 		public ICommand UpdateLabelButtonClick
 		{
 			get { return new RelayCommand(UpdateLabelExecute, CanUpdateLabelExecute); }
 		}
 
-        #region ExitApplicationClick
-        /// <summary>
-        /// Exits the application command.
-        /// </summary>
+		#region ExitApplicationClick
+		/// <summary>
+		/// Exits the application command.
+		/// </summary>
 		void ExitApplicationCommand()
 		{
 			Application.Current.Shutdown();
 		}
 
-        /// <summary>
-        /// Determines whether this instance [can exit application execute].
-        /// </summary>
-        /// <returns>
-        ///   <c>true</c> if this instance [can exit application execute]; otherwise, <c>false</c>.
-        /// </returns>
+		/// <summary>
+		/// Determines whether this instance [can exit application execute].
+		/// </summary>
+		/// <returns>
+		///   <c>true</c> if this instance [can exit application execute]; otherwise, <c>false</c>.
+		/// </returns>
 		bool CanExitApplicationExecute()
 		{
 			return true;
 		}
 
-        /// <summary>
-        /// Gets the exit application click.
-        /// </summary>
-        /// <value>
-        /// The exit application click.
-        /// </value>
+		/// <summary>
+		/// Gets the exit application click.
+		/// </summary>
+		/// <value>
+		/// The exit application click.
+		/// </value>
 		public ICommand ExitApplicationClick  //bind to "ExitApplicationClick" name in XAML
 		{
 			get { return new RelayCommand(ExitApplicationCommand, CanExitApplicationExecute); }
 		}
 
-        #endregion
-        #endregion
-       
-        /// <summary>
-        /// ///////////////////////WriteHistoryToFileClick
-        /// </summary>
-        /// 
+		#endregion
+		#endregion
+	   
+		/// <summary>
+		/// ///////////////////////WriteHistoryToFileClick
+		/// </summary>
+		///  
+		ObservableCollection<ListViewData> listViewCollection = new ObservableCollection<ListViewData>();
 
-        void WriteHistoryToFileClickCommand()
-        {
-            using (var writer = new StreamWriter(@"i:\test.txt"))
-            {
-                writer.WriteLine("No. \t a \t b \t c \t Discriminant \t Root1 \t Root2");
-                foreach (ListViewData item in ListViewCollection)
-                {
-                    writer.WriteLine(item.ListView_No + " \t" + item.ListView_a + " \t" + item.ListView_b + " \t" + item.ListView_c
-                                     + " \t" + item.ListView_Discriminant + " \t" + item.ListView_x1 
-                                     + " \t" + item.ListView_x2);
-                }
-            }
-        }
+		public ObservableCollection<ListViewData> ListViewCollection
+		{ get { return listViewCollection; } }
 
-      
-        bool CanWriteHistoryToFileClickExecute()
-        {
-            return true;
-        }
+		void WriteHistoryToFileClickCommand()
+		{
+			using (var writer = new StreamWriter(@"i:\test.txt"))
+			{
+				writer.WriteLine("No. \t a \t b \t c \t Discriminant \t Root1 \t Root2 \t Date and time");
+				foreach (ListViewData item in ListViewCollection)
+				{
+					writer.WriteLine(item.ListView_No + " \t" + item.ListView_a + " \t" + item.ListView_b + " \t" + item.ListView_c
+									 + " \t" + item.ListView_Discriminant + " \t" + item.ListView_x1 
+									 + " \t" + item.ListView_x2 + " \t " + item.ListView_Datetime.ToLongDateString() + " " + item.ListView_Datetime.ToLongTimeString());
+				}
+			}
+		}
 
-        
-        public ICommand WriteHistoryToFileClick
-        {
-            get { return new RelayCommand(WriteHistoryToFileClickCommand, CanWriteHistoryToFileClickExecute); }
-        }
+		bool CanWriteHistoryToFileClickExecute()
+		{
+			return true;
+		}
 
-        #endregion
+		public ICommand WriteHistoryToFileClick
+		{
+			get { return new RelayCommand(WriteHistoryToFileClickCommand, CanWriteHistoryToFileClickExecute); }
+		}
 
-        ObservableCollection<ListViewData> listViewCollection = new ObservableCollection<ListViewData>();
+		void WriteHistoryToDatabaseEntityClickCommand()
+		{
+			using (var db = new HistoryContext())
+			{
+				foreach (ListViewData item in ListViewCollection)
+				{
+					var hist = new History();
 
-        public ObservableCollection<ListViewData> ListViewCollection
-        { get { return listViewCollection; } }
+                    hist.Number = int.Parse(item.ListView_No);
+					hist.a = double.Parse(item.ListView_a);
+					hist.b = double.Parse(item.ListView_b);
+					hist.c = double.Parse(item.ListView_c);
+					hist.Discriminant = double.Parse(item.ListView_Discriminant);
+					hist.x1 = item.ListView_x1;
+					hist.x2 = item.ListView_x2;
+                    hist.dbDateTime = item.ListView_Datetime;
 
-       
-    }
+					db.Histories.Add(hist);
+				}
+				db.SaveChanges();
+			}
+		}
 
-    public class ListViewData
-    {
-        public string ListView_No { get; set; }
-        public string ListView_a { get; set; }
-        public string ListView_b { get; set; }
-        public string ListView_c { get; set; }
-        public string ListView_Discriminant { get; set; }
-        public string ListView_x1 { get; set; }
-        public string ListView_x2 { get; set; }
+		bool CanWriteHistoryToDatabaseEntityClickExecute()
+		{
+			return true;
+		}
 
-    }
+        public ICommand WriteHistoryToDatabaseEntityClick
+		{
+			get { return new RelayCommand(WriteHistoryToDatabaseEntityClickCommand, CanWriteHistoryToDatabaseEntityClickExecute); }
+		}
+
+
+		/// <summary>
+		/// ///////////////////WriteHistoryToDatabaseEntityClick
+		/// </summary>
+
+		#endregion
+	}
+
+	public class ListViewData
+	{
+		public string ListView_No { get; set; }
+		public string ListView_a { get; set; }
+		public string ListView_b { get; set; }
+		public string ListView_c { get; set; }
+		public string ListView_Discriminant { get; set; }
+		public string ListView_x1 { get; set; }
+		public string ListView_x2 { get; set; }
+        public DateTime ListView_Datetime { get; set; }
+	}
+
+	public class History
+	{
+		public int HistoryId { get; set; }                  //global counter
+        public int Number { get; set; }                     //local counter
+		public double a { get; set; }
+		public double b { get; set; }
+		public double c { get; set; }
+		public double Discriminant { get; set; }
+		public string x1 { get; set; }
+		public string x2 { get; set; }
+        public DateTime dbDateTime { get; set; }
+
+        //use DBCC LOG('Quadratic_equation_solver_WPF.HistoryContext', 3) in sql server management studio for db contents logs
+	}
+
+	public class HistoryContext : DbContext
+	{
+		public DbSet<History> Histories { get; set; }   
+	}
 }
-
 
